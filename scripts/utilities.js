b6e5be447a729cdfc32ee31212a81836c354e786
Badhon deb <badhondeb100@gmail.coml>
2025-05-08 03:59:32 +0600

    5/82025

---
+++ scripts/utilities.js
@@ -0,0 +1,12 @@
+function getInnerTextbyID(id) {
+    const value = document.getElementById(id).innerText;
+    const convertedValue = parseInt(value);
+    return convertedValue;
+}
+
+// function getInputbyid(id) {
+//     const value = document.getElementById(id).value;
+//     const convertedValue = parseInt(value);
+//     return convertedValue;
+// }
+
