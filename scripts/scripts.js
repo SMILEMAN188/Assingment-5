bf1c22943f3d9f30ad87c0af3be540970c581d40
Badhon deb <badhondeb100@gmail.coml>
2025-05-08 23:53:39 +0600

    mean

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,14 +2,14 @@ document.getElementById("btn-1").addEventListener("click",function (event) {
     event.preventDefault();
     const finishTask =  getInnerTextbyID("finish-task");
     const completeTask = getInnerTextbyID("completed-task");
-    
+    const container = getInnerTextbyID("container");
 
     alert("Board updated successfully");
     const minus = finishTask - 1;
     document.getElementById("finish-task").innerText=minus;
     const sum = completeTask + 1;
     document.getElementById("completed-task").innerText=sum
-    
+    const div = document.createElement("div")
     this.disabled=true ;
 
 })
